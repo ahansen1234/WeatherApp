e4f4a20e004aeea6c9f2b854dac4fb2b7f214e6c
Alexander Martinez Hansen <alex.c.hansen@gmail.com>
2023-08-09 11:08:10 -0700

    initializing weather app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { LocationSearch } from "./components/LocationSearch";
+import { LocationTable } from "./components/LocationTable";
+import { WeatherLocation } from "./model/Weather";
+import { searchLocation } from "./services/WeatherService";
+import { ErrorAlert, WarningAlert } from "./components/Alerts";
+import { WeatherSummary } from "./components/WeatherSummary";
 
 function App() {
+  const [locations, setLocations] = useState<WeatherLocation[]>([]);
+  const [currentLocation, setCurrentLocation] =
+    useState<WeatherLocation | null>(null);
+  const [error, setError] = useState("");
+  const [warning, setWarning] = useState("");
+
+  let addLocation = async (term: string) => {
+    resetAlerts();
+    const location = await searchLocation(term);
+
+    if (!location) {
+      setError(`No location found called '${term}'`);
+    } else if (locations.find((item) => item.id === location.id)) {
+      setWarning(`Location '${term}' is already in the list.`);
+    } else {
+      setLocations([location, ...locations]);
+    }
+  };
+
+  const resetAlerts = () => {
+    setError("");
+    setWarning("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Weather App</h1>
+      <h6>Enter and save cities to check their current weather, and forecasts for the next 24 hours!</h6>
+      <LocationSearch onSearch={addLocation} />
+      <ErrorAlert message={error} />
+      <WarningAlert message={warning} />
+      <LocationTable
+        locations={locations}
+        current={currentLocation}
+        onSelect={(location) => setCurrentLocation(location)}
+      />
+      <WeatherSummary location={currentLocation} />
     </div>
   );
 }
---
+++ src/components/WeatherSummary.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState, FC } from "react";
+import { Weather } from "../model/Weather";
+import { WeatherEntry } from "./WeatherEntry";
+import { WeatherLocation } from "../model/Weather";
+import { readWeather, readForecast } from "../services/WeatherService";
+import "./weatherSummary.css";
+
+interface WeatherSummaryProps {
+  location: WeatherLocation | null;
+}
+
+export const WeatherSummary: FC<WeatherSummaryProps> = ({ location }) => {
+  const [weather, setWeather] = useState<Weather | null>(null);
+  const [forecast, setForecast] = useState<Weather[] | null>(null);
+
+  useEffect(() => {
+    (async function () {
+      if (location) {
+        const [weather, forecast] = await Promise.all([
+          readWeather(location.id),
+          readForecast(location.id),
+        ]);
+        setWeather(weather);
+        setForecast(forecast);
+      }
+    })();
+  }, [location]);
+  if (!location || !weather || !forecast) return null;
+
+  return (
+    <div>
+      <hr />
+      <h2>{location.name}</h2>
+      <WeatherEntry weather={weather} />
+          <h2>Forecast for {location.name}</h2>
+      <div>
+        <ol className="outerList">
+          {forecast.map((timePoint) => (
+            <li key={timePoint.dt} className="lineItem">
+              <WeatherEntry weather={timePoint} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
